6f6f703924dfeb32845eabb1687b65416d2a7746
jacobobos <jokstu@ktu.lt>
2025-03-18 16:19:46 +0200

    Added changeable unit power coefficients

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,9 +21,12 @@ namespace tkiw_WaveRandomizer
         public MainWindow()
         {
             InitializeComponent();
-            densityAlgo_cbo.Items.Add("linear");
-            densityAlgo_cbo.SelectedItem = "linear";
+            filePath_tbx.Text = @"..\..\..\unit.csv";
+            densityAlgo_cbo.Items.Add("Linear");
+            densityAlgo_cbo.Items.Add("Normal");
+            densityAlgo_cbo.SelectedItem = "Linear";
             strengthAlgo_cbo.Items.Add("Default");
+            strengthAlgo_cbo.Items.Add("Hardcore");
             strengthAlgo_cbo.SelectedItem = "Default";
         }
 
@@ -45,15 +48,29 @@ namespace tkiw_WaveRandomizer
             //Build Wave_presets_village
             Dictionary<int, List<string>> csvRows = WavePresetString(enemyUnits, waveStrengths, densityAlgo_cbo.SelectedItem.ToString(), ref numOfColsOut);
         }
-        private static Dictionary<string, double> LoadEnemyUnits(string filePath)
+        private Dictionary<string, double> LoadEnemyUnits(string filePath)
         {
             Dictionary<string, double> enemyUnits = new Dictionary<string, double>();
             string[] fullFileContent = File.ReadAllLines(filePath);
             //string[] unitFileHeaders = fullFileContent[0].Split(',');                         could be used to replace fullFileContent[0].Split(','). feels not worth
             string[] lineArr = new string[fullFileContent[0].Split(',').Length];
             int unitIdCol = 0, unitHpCol = 0, unitDmgCol = 0, unitAtkSpdCol = 0, unitDpsCol = 0, unitFactionCol = 0;
-            double healthPower = 0.025;
-            double dpsPower = 0.4;
+            double healthPower = 0, dpsPower = 0;
+
+
+            //checks if the values given by the user are valid, if so saves them in healhPower and dpsPower
+            if (Double.TryParse(hpCoef_tbx.Text, out healthPower)){
+            }
+            else{
+                throw new Exception("Invalid input for health power.");
+            }
+            if (Double.TryParse(dmgCoef_tbx.Text, out dpsPower)){
+            }
+            else{
+                throw new Exception("Invalid input for dps power.");
+            }
+
+
 
             //get col indices
             for (int i = 0; i < fullFileContent[0].Split(',').Length; i++)
@@ -100,6 +117,13 @@ namespace tkiw_WaveRandomizer
                         120, 120, 210, 210, 210, 210, 210, 300, 300, 300,
                         300, 300, 300, 300, 165, 450
                     ]); break;
+                case "Hardcore":
+                    waveStrengths.AddRange([
+                        45, 45, 45, 45, 90, 90, 90, 90, 180, 180,
+                        180, 180, 180, 180, 180, 180, 180, 180, 180, 360,
+                        360, 360, 420, 420, 420, 420, 420, 600, 600, 600,
+                        600, 600, 600, 600, 330, 900
+                    ]);break;
                 default:
                     throw new Exception("Failed to provide acceptable Wave Strength Algo");
             }
@@ -119,14 +143,20 @@ namespace tkiw_WaveRandomizer
                 //check what desity method we will use to determine the number of units we want
                 switch (densityAlgo)
                 {
-                    case "linear":
-                        unitTarget = TotalUnitTargetLinear(wave); break;
-                    default :
+                    case "Linear":
+                        unitTarget = TotalUnitTargetLinear(wave);
+                        break;
+                    case "Normal":
+                        throw new Exception("Density Algo not implamented");
+                    //unitTarget = TotalUnitTargetNormal(wave);
+                    //break;
+                    default:
                         throw new Exception("Density Algo not given");
                 }
+
                 //generate a unit along a normal distribtion of unit target over wave stregth
                 unitMeanStrength = wave / Convert.ToDouble(unitTarget);
-                for (double i = 0; i < wave - wave*.10;)
+                for (double i = 0; i < wave - wave*.10;)//runs until 90% of the wave is filled
                 {
                     if (csvOut.ContainsKey(key))
                     {
@@ -150,8 +180,37 @@ namespace tkiw_WaveRandomizer
         //TODO
         private static string GenNormalDistroUnit(Dictionary<string, double> enemyUnits, double unitMeanStrength)
         {
+
             //TODO Build method to pull unit based on a normal distro around unitMeanStrength
+            double stdDev = 1;
+            double unitpower = NextDistributed(unitMeanStrength, stdDev, BoxMuller);
+            //find unit closest to unitpower and return that unit
             return "";
         }
+
+        //func double double doube means it needs a method that takes 2 doubles and returns a double
+        public static double NextDistributed(double mean, double stdDev, Func<double, double, double> transform)
+        {
+            var transformed = double.NaN;
+            Random _random = new Random();
+            // this loop just allows us to adapt to any transform function which 
+            // may provide invalid results based on random inputs.
+            while (double.IsNaN(transformed))
+            {
+                // generate uniform randoms and pass to the supplied distribution
+                // transform.
+                var uniform1 = 1.0 - _random.NextDouble();
+                var uniform2 = 1.0 - _random.NextDouble();
+                transformed = transform(uniform1, uniform2);
+            }
+
+            // scale/shift by mean/std dev
+            return mean + stdDev * transformed;
+        }
+
+        // Box-muller transform - produces normal dist val based on 2
+        // uniformly distributed randoms
+        public static double BoxMuller(double u1, double u2) =>
+            System.Math.Sqrt(-2.0 * System.Math.Log(u1)) * System.Math.Sin(2.0 * System.Math.PI * u2);
     }
 }
